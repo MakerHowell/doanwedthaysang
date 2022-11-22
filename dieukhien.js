e61aad3502a48a7a74870dcf1f2d51695da5a7c7
MakerHowell <nguhuukien01@gmail.com>
2022-11-22 07:56:10 +0700

    #anh

--- dieukhien.js
+++ dieukhien.js
@@ -1030,3 +1030,11 @@ function hienthanhtoan(IDdh){
     </div>`;
     document.getElementById('popup').innerHTML=giohang;
 }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
